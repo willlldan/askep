f5bbec99b82e299844ae93b454ad31c2a53154aa
Wildan Fauzi Rakhman <173040071@mail.unpas.ac.id>
2026-06-09 12:46:15 +0700

    Fixing cetak format anggrek

--- cetak_form/template_pdf_format_anggrek.php
+++ cetak_form/template_pdf_format_anggrek.php
@@ -1536,23 +1536,22 @@ function safe_p($var) {
         <!-- ================================ -->
         <!-- SECTION 6: CATATAN KEPERAWATAN -->
         <!-- ================================ -->
-        <h3 class="mt-5">Diagnosa Keperawatan</h3>
-
+        <h3 class="mt-5">Diagnosa Keperawatan Prioritas</h3>
         <table class="data">
             <thead>
                 <tr>
-                    <th>Diagnosa</th>
-                    <th>Tanggal Ditemukan</th>
-                    <th>Tanggal Teratasi</th>
+                    <th width="5%">No.</th>
+                    <th width="40%">DO/DS</th>
+                    <th>Diagnosa Keperawatan</th>
                 </tr>
             </thead>
             <tbody>
                 <?php if (!empty($lainnya['diagnosa'])): ?>
-                    <?php foreach ($lainnya['diagnosa'] as $dx): ?>
+                    <?php foreach ($lainnya['diagnosa'] as $i => $dx): ?>
                         <tr>
-                            <td><?= p($dx['diagnosa']) ?></td>
-                            <td><?= p($dx['tgl_ditemukan']) ?></td>
-                            <td><?= p($dx['tgl_teratasi']) ?></td>
+                            <td><?= $i + 1 ?></td>
+                            <td><?= p($dx['dods']) ?></td>
+                            <td><?= p($dx['diagnosa_kep']) ?></td>
                         </tr>
                     <?php endforeach; ?>
                 <?php else: ?>
@@ -1563,42 +1562,42 @@ function safe_p($var) {
             </tbody>
         </table>
 
-        <h3 class="mt-5">Intervensi Keperawatan</h3>
-
+        <h3 class="mt-5">Rencana Keperawatan</h3>
         <table class="data">
             <thead>
                 <tr>
-                    <th width="40%">Diagnosa</th>
-                    <th width="30%">Tujuan dan Kriteria Hasil</th>
+                    <th width="5%">No.</th>
+                    <th width="35%">Diagnosa Keperawatan</th>
+                    <th width="30%">Tujuan &amp; Kriteria Hasil</th>
                     <th width="30%">Intervensi</th>
                 </tr>
             </thead>
             <tbody>
-                <?php if (!empty($lainnya['intervensi'])): ?>
-                    <?php foreach ($lainnya['intervensi'] as $inv): ?>
+                <?php if (!empty($lainnya['rencana'])): ?>
+                    <?php foreach ($lainnya['rencana'] as $i => $r): ?>
                         <tr>
-                            <td><?= p($inv['diagnosa']) ?></td>
-                            <td><?= p($inv['tujuan_kriteria']) ?></td>
-                            <td><?= p($inv['intervensi']) ?></td>
+                            <td><?= $i + 1 ?></td>
+                            <td><?= p($r['diagnosa']) ?></td>
+                            <td><?= p($r['tujuan']) ?></td>
+                            <td><?= p($r['intervensi']) ?></td>
                         </tr>
                     <?php endforeach; ?>
                 <?php else: ?>
                     <tr>
-                        <td colspan="3" style="text-align:center">-</td>
+                        <td colspan="4" style="text-align:center">-</td>
                     </tr>
                 <?php endif; ?>
             </tbody>
         </table>
 
         <h3 class="mt-5">Implementasi Keperawatan</h3>
-
         <table class="data">
             <thead>
                 <tr>
-                    <th>No. DX</th>
-                    <th>Hari/Tanggal</th>
-                    <th>Jam</th>
-                    <th>Implementasi</th>
+                    <th width="8%">No. DX</th>
+                    <th width="18%">Hari/Tanggal</th>
+                    <th width="10%">Jam</th>
+                    <th>Implementasi &amp; Hasil</th>
                 </tr>
             </thead>
             <tbody>
@@ -1608,7 +1607,7 @@ function safe_p($var) {
                             <td><?= p($impl['no_dx']) ?></td>
                             <td><?= p($impl['hari_tgl']) ?></td>
                             <td><?= p($impl['jam']) ?></td>
-                            <td><?= p($impl['implementasi']) ?></td>
+                            <td style="white-space: pre-wrap;"><?= nl2br(p($impl['implementasi_hasil'])) ?></td>
                         </tr>
                     <?php endforeach; ?>
                 <?php else: ?>
@@ -1620,17 +1619,13 @@ function safe_p($var) {
         </table>
 
         <h3 class="mt-5">Evaluasi Keperawatan</h3>
-
         <table class="data">
             <thead>
                 <tr>
-                    <th>No. DX</th>
-                    <th>Hari/Tanggal</th>
-                    <th>Jam</th>
-                    <th>S</th>
-                    <th>O</th>
-                    <th>A</th>
-                    <th>P</th>
+                    <th width="8%">No. DX</th>
+                    <th width="18%">Hari/Tanggal</th>
+                    <th width="10%">Jam</th>
+                    <th>Evaluasi (SOAP)</th>
                 </tr>
             </thead>
             <tbody>
@@ -1640,15 +1635,12 @@ function safe_p($var) {
                             <td><?= p($eval['no_dx']) ?></td>
                             <td><?= p($eval['hari_tgl']) ?></td>
                             <td><?= p($eval['jam']) ?></td>
-                            <td><?= p($eval['evaluasi_s']) ?></td>
-                            <td><?= p($eval['evaluasi_o']) ?></td>
-                            <td><?= p($eval['evaluasi_a']) ?></td>
-                            <td><?= p($eval['evaluasi_p']) ?></td>
+                            <td style="white-space: pre-wrap;"><?= nl2br(p($eval['evaluasi_soap'])) ?></td>
                         </tr>
                     <?php endforeach; ?>
                 <?php else: ?>
                     <tr>
-                        <td colspan="7" style="text-align:center">-</td>
+                        <td colspan="4" style="text-align:center">-</td>
                     </tr>
                 <?php endif; ?>
             </tbody>
